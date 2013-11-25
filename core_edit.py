75ca6ba4a02fdb3c55d149b3ce4f4fc342c1c326
reus <reusee@ymail.com>
2013-11-25 22:22:34 +0800

    selection deletion

--- core_edit.py
+++ core_edit.py
@@ -1,4 +1,13 @@
 class Edit:
   def __init__(self):
+
     self.emit('bind-command-key', 'i', self.enter_edit_mode)
+    self.emit('bind-command-key', 'd', self.delete)
+
     self.emit('bind-edit-key', 'k d', self.enter_command_mode)
+
+  def delete(self, view):
+    buf = view.get_buffer()
+    if buf.get_has_selection():
+      buf.delete_selection(True, True)
+      self.toggle_char_selection(view)
