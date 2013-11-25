65b58dfccbfb3a31fa16609eae9f24774cc14380
reus <reusee@ymail.com>
2013-11-25 16:46:11 +0800

    refactor modal module

--- core_edit.py
+++ core_edit.py
@@ -1,10 +1,3 @@
 class Edit:
   def __init__(self):
     self.emit('bind-command-key', 'i', self.enter_edit_mode)
-
-    self.new_signal('edit-mode-entered', ())
-
-  def enter_edit_mode(self):
-    self.operation_mode = self.EDIT
-    self.reset_key_handler(self.edit_key_handler)
-    self.emit('edit-mode-entered')
--- core_modal.py
+++ core_modal.py
@@ -7,39 +7,39 @@ class Modal:
   NONE, CHAR, LINE, RECT = range(4)
 
   def __init__(self):
-    self.connect('view-created', lambda editor, view:
-        view.connect('key-press-event', self.handle_key_press))
-
-    self.new_signal('bind-command-key', (str, object))
-    self.connect('bind-command-key', self.bind_command_key)
-
-    self.new_signal('command-mode-entered', ())
 
     self.operation_mode = self.COMMAND
     self.selection_mode = self.NONE
     self.command_key_handler = {}
     self.edit_key_handler = {}
 
+    self.connect('view-created', lambda editor, view:
+        view.connect('key-press-event', self.handle_key_press))
+
+    self.new_signal('bind-command-key', (str, object))
+    self.new_signal('bind-edit-key', (str, object))
+    self.connect('bind-command-key', 
+        lambda _, seq, handler: self.bind_key_handler(self.command_key_handler, seq, handler))
+    self.connect('bind-edit-key',
+        lambda _, seq, handler: self.bind_key_handler(self.edit_key_handler, seq, handler))
+
+    self.new_signal('edit-mode-entered', ())
+    self.new_signal('command-mode-entered', ())
+
     self.key_handler = self.command_key_handler
-    self.delay_events = []
     self.n = 0
 
     for i in range(0, 10):
       self.emit('bind-command-key', str(i), self.make_number_prefix_handler(i))
 
   def handle_key_press(self, view, ev):
-    if self.operation_mode == self.COMMAND:
-      return self.handle_command_key(view, ev)
-    elif self.operation_mode == self.EDIT:
-      return self.handle_edit_key(view, ev)
-    not_here
-
-  def handle_command_key(self, view, ev):
     _, val = ev.get_keyval()
     if val == Gdk.KEY_Shift_L or val == Gdk.KEY_Shift_R: return False
     if val == Gdk.KEY_Escape: # cancel command
-      self.reset_key_handler(self.command_key_handler)
+      self.enter_command_mode()
       return True
+    is_command_mode = self.operation_mode == self.COMMAND
+    is_edit_mode = self.operation_mode == self.EDIT
     handler = None
     if isinstance(self.key_handler, dict): # dict handler
       key = chr(val) if val > 0x20 and val <= 0x7e else val
@@ -49,7 +49,11 @@ class Modal:
     elif callable(self.key_handler): # function handler
       handler = self.key_handler
     if callable(handler): # trigger a command or call handler function
-      self.reset_key_handler(self.command_key_handler)
+      if is_command_mode:
+        self.reset_key_handler(self.command_key_handler)
+      else:
+        #TODO stop timer
+        self.reset_key_handler(self.edit_key_handler)
       ret = self.execute_key_handler(handler, view, ev)
       if callable(ret): # another function handler
         self.key_handler = ret
@@ -59,16 +63,21 @@ class Modal:
         pass
     elif isinstance(handler, dict): # sub dict handler
       self.key_handler = handler
+      if is_edit_mode:
+        #TODO save keyval start timer
+        pass
     else: # no handler
-      self.reset_key_handler(self.command_key_handler)
-      print('no command for', chr(val))
+      if is_command_mode:
+        self.reset_key_handler(self.command_key_handler)
+        print('no command for', chr(val))
+      else:
+        #TODO stop timer
+        #TODO insert delayed chars
+        self.reset_key_handler(self.edit_key_handler)
+        return False
     return True
 
-  def handle_edit_key(self, view, key):
-    return False
-
   def reset_key_handler(self, handler):
-    self.delay_events.clear()
     self.key_handler = handler
 
   def execute_key_handler(self, f, view, ev):
@@ -84,9 +93,8 @@ class Modal:
       else: print(param); handler_error
     return f(*args)
 
-  def bind_command_key(self, editor, seq, handler):
+  def bind_key_handler(self, cur, seq, handler):
     seq = seq.split(' ')
-    cur = self.command_key_handler
     for key in seq[:len(seq) - 1]:
       if key not in cur:
         cur[key] = {}
@@ -102,3 +110,13 @@ class Modal:
       self.n = self.n * 10 + i
       return 'is_number_prefix'
     return f
+
+  def enter_command_mode(self):
+    self.operation_mode = self.COMMAND
+    self.reset_key_handler(self.command_key_handler)
+    self.emit('command-mode-entered')
+
+  def enter_edit_mode(self):
+    self.operation_mode = self.EDIT
+    self.reset_key_handler(self.edit_key_handler)
+    self.emit('edit-mode-entered')
