9d257ceaed410ca147c12983a0a519da76fe469e
reus <reusee@ymail.com>
2013-11-26 22:03:08 +0800

    line selection mode

--- core_move.py
+++ core_move.py
@@ -31,6 +31,13 @@ class Move:
   def move_mark(self, buf, it):
     if self.selection_mode == self.NONE:
       buf.place_cursor(it)
+    elif self.selection_mode == self.LINE:
+      cur = buf.get_iter_at_mark(buf.get_insert())
+      if cur.compare(it) == -1: # forward
+          if not it.starts_line(): it.forward_line()
+      else: # backward
+          if not it.starts_line(): it.set_line_offset(0)
+      buf.move_mark(buf.get_insert(), it)
     else:
       buf.move_mark(buf.get_insert(), it)
 
