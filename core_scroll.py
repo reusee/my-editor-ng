bda31116eec541a83a02c20a7b44dd95514b76a1
reus <reusee@ymail.com>
2013-11-26 22:53:21 +0800

    add command: zt zb zz

--- core_scroll.py
+++ core_scroll.py
@@ -2,6 +2,9 @@ class Scroll:
   def __init__(self):
     self.emit('bind-command-key', 'M', self.page_down)
     self.emit('bind-command-key', 'U', self.page_up)
+    self.emit('bind-command-key', 'z t', lambda view: self.scroll_cursor(view, 0, 0))
+    self.emit('bind-command-key', 'z b', lambda view: self.scroll_cursor(view, 0, 1))
+    self.emit('bind-command-key', 'z z', lambda view: self.scroll_cursor(view, 0, 0.5))
 
   def page_down(self, view):
     alloc = view.get_allocation()
@@ -16,3 +19,7 @@ class Scroll:
     it = view.get_line_at_y(view.get_vadjustment().get_value() - alloc.height + 50)[0]
     self.move_mark(buf, it)
     view.scroll_to_mark(buf.get_insert(), 0, True, 0, 0)
+
+  def scroll_cursor(self, view, x, y):
+    buf = view.get_buffer()
+    view.scroll_to_mark(buf.get_insert(), 0, True, x, y)
