d42c0df3804a735d3eb27fb6465305dfa556b8a8
reus <reusee@ymail.com>
2013-11-29 20:12:37 +0800

    remember buffer cursor position between view switching

--- core_switcher.py
+++ core_switcher.py
@@ -2,6 +2,25 @@ class Switcher:
     def __init__(self):
         self.emit('bind-command-key', '>', self.switch_next_buffer)
         self.emit('bind-command-key', '<', self.switch_prev_buffer)
+        self.connect('view-created', self.setup_switcher)
+
+    def setup_switcher(self, _, view):
+      view.attr['buffer-position'] = {}
+      view.connect('focus-out-event', self.save_buffer_position)
+      view.connect('focus-in-event', self.restore_buffer_position)
+
+    def save_buffer_position(self, view, ev):
+      buf = view.get_buffer()
+      mark = buf.create_mark(None, buf.get_iter_at_mark(buf.get_insert()))
+      view.attr['buffer-position'][buf] = mark
+
+    def restore_buffer_position(self, view, ev):
+      buf = view.get_buffer()
+      if buf in view.attr['buffer-position']:
+        mark = view.attr['buffer-position'][buf]
+        buf.place_cursor(buf.get_iter_at_mark(mark))
+        buf.delete_mark(mark)
+        del view.attr['buffer-position'][buf]
 
     def switch_next_buffer(self, view):
         index = self.buffers.index(view.get_buffer())
