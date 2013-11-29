1135544309f741fdd8406b02e00151d275e0a1eb
reus <reusee@ymail.com>
2013-11-29 19:31:52 +0800

    refactor search module

--- core_search.py
+++ core_search.py
@@ -1,109 +1,103 @@
-from gi.repository import Gtk, Gdk, GObject
+from gi.repository import Gtk, Gdk, GObject, GtkSource
 
 class Search:
   def __init__(self):
     self.search_entry = SearchEntry()
-    self.search_entry.set_hexpand(True)
     self.connect('realize', lambda _:
       self.south_area.attach(self.search_entry, 0, -1, 1, 1))
+
+    self.connect('buffer-created', self.setup_search)
+
+    self.search_entry.connect('update', self.on_search_entry_update)
+    self.search_entry.connect('done', lambda entry:
+      self.next_search_result(entry.view, entry.is_backward))
+
     self.emit('bind-command-key', '/',
-      lambda view: self.search_entry.run(view, is_backward = False))
+      lambda view: self.search_entry.run(view))
     self.emit('bind-command-key', '?',
       lambda view: self.search_entry.run(view, is_backward = True))
+    self.emit('bind-command-key', 'n', lambda view:
+      self.next_search_result(view))
+    self.emit('bind-command-key', 'm', lambda view:
+      self.next_search_result(view, backward = True))
 
-    self.connect('buffer-created', self.setup_searcher)
-
-    self.search_entry.connect('notify::text', lambda entry, _:
-      entry.view.get_buffer().attr['searcher']
-        .set_property('pattern', entry.get_text()))
-    self.search_entry.connect('done', lambda entry:
-      self.next_search_result(entry.view)
-      if not entry.is_backward else
-      self.prev_search_result(entry.view))
-
-    self.emit('bind-command-key', 'n', self.next_search_result)
-    self.emit('bind-command-key', 'm', self.prev_search_result)
-
-  def setup_searcher(self, _, buf):
-    buf.attr['searcher'] = Searcher(buf)
+  def setup_search(self, _, buf):
     tag = buf.create_tag('search-result',
       background = '#0099CC', foreground = '#000000')
     buf.attr['search-result-tag'] = tag
+    buf.attr['search-pattern'] = ''
+    buf.connect('changed', self.update_search_result)
 
-  def next_search_result(self, view):
-    buf = view.get_buffer()
-    it = buf.get_iter_at_mark(buf.get_insert())
-    tag = buf.attr['search-result-tag']
-    if it.forward_to_tag_toggle(tag):
-      if it.ends_tag(tag):
-        if it.forward_to_tag_toggle(tag):
-          self.move_mark(buf, it)
-      else:
-        self.move_mark(buf, it)
-    view.scroll_to_mark(buf.get_insert(), 0, True, 1, 0.5)
-
-  def prev_search_result(self, view):
-    buf = view.get_buffer()
-    it = buf.get_iter_at_mark(buf.get_insert())
-    tag = buf.attr['search-result-tag']
-    if it.backward_to_tag_toggle(tag):
-      if it.ends_tag(tag):
-        if it.backward_to_tag_toggle(tag):
-          self.move_mark(buf, it)
-      else:
-        self.move_mark(buf, it)
-    view.scroll_to_mark(buf.get_insert(), 0, True, 1, 0.5)
-
-class Searcher(GObject.GObject):
-  __gsignals__ = {
-  }
-  pattern = GObject.property(type = str, default = '')
-  def __init__(self, buf):
-    GObject.GObject.__init__(self)
-    self.buf = buf
-    self.connect('notify::pattern', self.update)
-
-  def update(self, _self, _):
-    self.buf.remove_tag_by_name('search-result',
-      self.buf.get_start_iter(), self.buf.get_end_iter())
-    pattern = self.pattern
-    buffer_end = self.buf.get_end_iter()
-    it = self.buf.get_start_iter()
+  def update_search_result(self, buf):
+    buf.remove_tag_by_name('search-result',
+      buf.get_start_iter(), buf.get_end_iter())
+    pattern = buf.attr['search-pattern']
+    if not pattern: return
+    buffer_end = buf.get_end_iter()
+    it = buf.get_start_iter()
     res = it.forward_search(pattern, 0, buffer_end)
     while res:
       start, end = res
-      self.buf.apply_tag_by_name('search-result', start, end)
+      buf.apply_tag_by_name('search-result', start, end)
       it = end
       res = it.forward_search(pattern, 0, buffer_end)
+
+  def on_search_entry_update(self, entry, buf):
+    self.update_search_result(buf)
+    it = buf.get_iter_at_mark(buf.get_insert())
+    tag = buf.attr['search-result-tag']
+    func = it.forward_to_tag_toggle
+    if entry.is_backward: func = it.backward_to_tag_toggle
+    if func(tag):
+      entry.view.scroll_to_iter(it, 0, True, 1, 0.5)
+
+  def next_search_result(self, view, backward = False):
+    buf = view.get_buffer()
+    it = buf.get_iter_at_mark(buf.get_insert())
+    tag = buf.attr['search-result-tag']
+    func = it.forward_to_tag_toggle
+    if backward: func = it.backward_to_tag_toggle
+    if func(tag):
+      if it.ends_tag(tag):
+        if func(tag):
+          self.move_mark(buf, it)
+      else:
+        self.move_mark(buf, it)
+    view.scroll_to_mark(buf.get_insert(), 0, True, 1, 0.5)
 
 class SearchEntry(Gtk.Entry):
 
   __gsignals__ = {
+    'update': (GObject.SIGNAL_RUN_FIRST, None, (GtkSource.Buffer,)),
     'done': (GObject.SIGNAL_RUN_FIRST, None, ()),
   }
 
   def __init__(self):
     super().__init__()
-    self.view = None
+    self.set_hexpand(True)
     self.connect('key-press-event', self.on_key_press_event)
+    self.connect('notify::text', self.update)
+
     self.is_backward = False
+    self.view = None
+
+  def update(self, _self, _):
+    self.view.get_buffer().attr['search-pattern'] = self.get_text()
+    self.emit('update', self.view.get_buffer())
 
   def run(self, view, is_backward = False):
     self.view = view
     self.is_backward = is_backward
     self.show_all()
     self.grab_focus()
+    buf = self.view.get_buffer()
 
   def on_key_press_event(self, _, ev):
     _, val = ev.get_keyval()
-    if val == Gdk.KEY_Escape:
-      self.keyword = ''
-      self.done()
-    elif val == Gdk.KEY_Return:
-      self.keyword = self.get_text()
-      self.done()
-
-  def done(self):
-    self.hide()
-    self.view.grab_focus()
-    self.emit('done')
+    if val == Gdk.KEY_Escape or val == Gdk.KEY_Return: # cancel
+      if val == Gdk.KEY_Escape:
+        self.view.scroll_to_mark(self.view.get_buffer().get_insert(), 0, True, 1, 0.5)
+      else:
+        self.emit('done')
+      self.hide()
+      self.view.grab_focus()
