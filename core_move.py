2609b6880e5a2715cadc238293f8822251c00eb6
reus <reusee@ymail.com>
2013-12-02 18:45:43 +0800

    fix moving bug

--- core_move.py
+++ core_move.py
@@ -49,10 +49,10 @@ class Move:
             for i in range(n): view.forward_display_line(it)
         else:
             for i in range(n): view.backward_display_line(it)
-        bytes_in_line = it.get_bytes_in_line()
+        chars_in_line = it.get_chars_in_line() - 1
         offset = buf.attr['current_offset']
-        if bytes_in_line <= buf.attr['current_offset']:
-            offset = bytes_in_line - 1
+        if offset > chars_in_line:
+            offset = chars_in_line
         if offset >= 0:
             it.set_line_offset(offset)
         buf.attr['freeze'] = True
