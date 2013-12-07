6487bbd5a2c96e10118905ace6117bed328f3119
reus <reusee@ymail.com>
2013-12-07 15:40:07 +0800

    fix input pattern matching

--- core_pattern_match.py
+++ core_pattern_match.py
@@ -5,22 +5,24 @@ class CorePatternMatch:
     def setup_pattern_matcher(self, _, buf):
         buf.attr['patterns'] = {}
         buf.attr['pattern-matcher-states'] = []
-        buf.connect('insert-text', self.update_matcher_state_on_insert)
-        buf.connect('delete-range', self.update_matcher_state_on_delete)
+        buf.connect_after('insert-text', self.update_matcher_state_on_insert)
+        buf.connect_after('delete-range', self.update_matcher_state_on_delete)
 
-        self.add_pattern(buf, 'foo', lambda _: True, lambda _: print('here'))
+        self.add_pattern(buf, 'foobar', lambda buf: print('foobar'))
 
     def update_matcher_state_on_insert(self, buf, it, text, length):
         new_states = []
-        # update current state
-        # new state
         states = buf.attr['pattern-matcher-states']
         for state in states:
             if text in state:
                 state = state[text]
-                if isinstance(state, tuple): # stop state
-                    predict, callback = state
-                    if predict(buf): callback(buf)
+                if callable(state):
+                    m = buf.create_mark(None, it, True)
+                    state(buf)
+                    buf.attr['pattern-matcher-states'].clear()
+                    it.assign(buf.get_iter_at_mark(m))
+                    buf.delete_mark(m)
+                    return
                 else:
                     new_states.append(state)
 
@@ -29,12 +31,11 @@ class CorePatternMatch:
             new_states.append(patterns[text])
 
         buf.attr['pattern-matcher-states'] = new_states
-        print(new_states)
 
     def update_matcher_state_on_delete(self, buf, start, end):
         buf.attr['pattern-matcher-states'].clear()
 
-    def add_pattern(self, buf, pattern, predict, callback):
+    def add_pattern(self, buf, pattern, callback):
         path = [c for c in pattern]
         cur = buf.attr['patterns']
         for c in path[:-1]:
@@ -48,4 +49,4 @@ class CorePatternMatch:
         key = path[-1]
         if key in cur: # conflict
             raise Exception('pattern conflict', pattern)
-        cur[key] = (predict, callback)
+        cur[key] = callback
