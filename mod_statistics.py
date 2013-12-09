c177882b312e7c23df77d38e638c34b08371f7ad
reus <reusee@gmail.com>
2013-12-09 20:00:56 +0800

    log invoked command to file

--- mod_statistics.py
+++ mod_statistics.py
@@ -1,13 +1,26 @@
+from gi.repository import Gdk
+import os
+
 class ModStatistics:
     def __init__(self, editor):
+        self.editor = editor
         editor.connect('key-pressed', self.collect_key)
         editor.connect('key-done', self.key_reseted)
         self.current_keys = []
+        self.log_file = open(os.path.join(
+            os.path.dirname(__file__),
+            'command-log'), 'a+')
 
     def collect_key(self, _, view, event):
-        #print(chr(event.get_keyval()[1]))
-        pass
+        if self.editor.operation_mode != self.editor.COMMAND: return
+        val = event.get_keyval()[1]
+        if not (val >= 0x20 and val <= 0x7e): return
+        self.current_keys.append(chr(val))
 
     def key_reseted(self, _):
-        #print('reset')
-        pass
+        keys = ''.join(self.current_keys)
+        if not keys: return
+        print('COMMAND', keys)
+        self.current_keys.clear()
+        self.log_file.write(keys + '\n')
+        self.log_file.flush()
