ee79007288d232f6614eedee689475b483cc60f9
reus <reusee@gmail.com>
2013-12-12 20:10:07 +0800

    show message when no action can be undo

--- core_edit.py
+++ core_edit.py
@@ -61,6 +61,8 @@ class CoreEdit:
             buf.undo()
             buf.place_cursor(buf.get_iter_at_mark(buf.get_insert()))
             view.scroll_to_mark(buf.get_insert(), 0, True, 1, 0.5)
+        else:
+            self.show_message('no undo action')
 
     def redo(self, view):
         buf = view.get_buffer()
