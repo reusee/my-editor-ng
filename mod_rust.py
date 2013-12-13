cc7f516fb4bc9ad2e1a8520239998b1c2d04cbd4
reus <reusee@gmail.com>
2013-12-14 00:37:52 +0800

    mod_rust: use snippet to insert braces

--- mod_rust.py
+++ mod_rust.py
@@ -11,7 +11,8 @@ class ModRust:
         buf.attr['is-word-char-func'] = self.is_word_char
         buf.attr['word-regex'] = regex.compile('[a-zA-Z0-9-_!]+')
 
-        self.editor.add_pattern(buf, '{{', self.insert_braces,
+        self.editor.add_pattern(buf, '{{', lambda buf:
+            self.editor.insert_snippet(buf, '{\n$1\n}'),
             drop_key_event = True, clear_matched_text = True)
 
     def is_word_char(self, c):
@@ -20,19 +21,3 @@ class ModRust:
         if c.isdigit(): return True
         if c in {'-', '_', '!'}: return True
         return False
-
-    def insert_braces(self, buf):
-        it = buf.get_iter_at_mark(buf.get_insert())
-        start = it.copy()
-        buf.begin_user_action()
-        # count indent
-        it.set_line_offset(0)
-        while not it.ends_line() and it.get_char().isspace():
-            it.forward_char()
-        indent_str = ' ' * it.get_line_offset()
-        # insert
-        buf.insert(start, '{\n%s\n%s}' % (indent_str, indent_str), -1)
-        start.backward_line()
-        start.set_line_offset(len(indent_str))
-        buf.place_cursor(start)
-        buf.end_user_action()
