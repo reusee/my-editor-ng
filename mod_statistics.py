0676367650bb013f58487fe380d01a5311ef02d3
reus <reusee@gmail.com>
2013-12-13 17:21:01 +0800

    log key handler invoke

--- mod_statistics.py
+++ mod_statistics.py
@@ -4,12 +4,18 @@ import os
 class ModStatistics:
     def __init__(self, editor):
         self.editor = editor
+
         editor.connect('key-pressed', self.collect_key)
         editor.connect('key-done', self.key_reseted)
         self.current_keys = []
-        self.log_file = open(os.path.join(
+        self.command_log_file = open(os.path.join(
             os.path.dirname(__file__),
             'command-log'), 'a+')
+
+        editor.connect('key-handler-execute', self.collect_handler)
+        self.handler_log_file = open(os.path.join(
+            os.path.dirname(__file__),
+            'handler-log'), 'a+')
 
     def collect_key(self, _, view, event):
         if self.editor.operation_mode != self.editor.COMMAND: return
@@ -21,5 +27,10 @@ class ModStatistics:
         keys = ''.join(self.current_keys)
         if not keys: return
         self.current_keys.clear()
-        self.log_file.write(keys + '\n')
-        self.log_file.flush()
+        self.command_log_file.write(keys + '\n')
+        self.command_log_file.flush()
+
+    def collect_handler(self, _, f, args):
+        self.handler_log_file.write(f.__dict__['_description_'])
+        self.handler_log_file.write('\t' + str(args) + '\n')
+        self.handler_log_file.flush()
